ce16af4d39ba45fb000eaece4f35e96088cf12d2
darkloor <rdg2250@yahoo.com>
2021-02-24 17:01:36 -0500

    comment under main

--- CS320_Project_Database/main/project_database/main/Main.java
+++ CS320_Project_Database/main/project_database/main/Main.java
@@ -6,6 +6,7 @@ import org.eclipse.jetty.server.Server;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
+		// Testing comment
 		String webappCodeBase = "./war";
 		File warFile = new File(webappCodeBase);
 		Launcher launcher = new Launcher();
@@ -23,10 +24,10 @@ public class Main {
 		
 		// Inform user that server is running
 		System.out.println("RUNNING: web server on port 8081");
-		
         // The use of server.join() the will make the current thread join and
         // wait until the server is done executing.
         // See http://docs.oracle.com/javase/7/docs/api/java/lang/Thread.html#join()
+		
 		server.join();
 	}
 }
