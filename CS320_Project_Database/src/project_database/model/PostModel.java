422e6dd21583621abd2e47e529b6a616e9e173ed
Ryan Black <rblack5@ycp.edu>
2021-04-02 23:30:32 -0400

    post model, minor change
    added a post_id field. This will be used to differentiate between the posts when displaying on the home page and loading specific posts

--- CS320_Project_Database/src/project_database/model/PostModel.java
+++ CS320_Project_Database/src/project_database/model/PostModel.java
@@ -2,6 +2,7 @@ package project_database.model;
 
 public class PostModel {
 	private String title, body;
+	int post_id;
 	public PostModel() {
 		
 	}
@@ -20,6 +21,14 @@ public class PostModel {
 	
 	public String getBody() {
 		return this.body;
+	}
+	
+	public void setPostID(int post_id) {
+		this.post_id = post_id;
+	}
+	
+	public int getPostID() {
+		return this.post_id;
 	}
 	
 }
