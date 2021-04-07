7967189804c127e05400741527c0d3caf2cc80bc
darkloor <rdg2250@yahoo.com>
2021-04-07 12:09:57 -0400

    login controller test done

--- CS320_Project_Database/junit/project_database/controller/LoginControllerTest.java
+++ CS320_Project_Database/junit/project_database/controller/LoginControllerTest.java
@@ -1,5 +1,45 @@
 package project_database.controller;
 
-public class LoginControllerTest {
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import project_database.model.UserModel;
+
+public class LoginControllerTest {
+	private UserModel model, model2;
+	private LoginController controller;
+	
+	@Before
+	public void setUp() {
+		model = new UserModel();
+		controller = new LoginController();
+	}
+	
+	@Test
+	public void testSetModel() {
+		model2 = new UserModel();
+		model2.setUsername("hi");
+		controller.setModel(model2);
+		assert(model2.getUsername().equals("hi"));
+		controller.setModel(model);
+	}
+	
+	@Test
+	public void testCheckLogIn() {
+		model.setUsername("user");
+		model.setPassword("password");
+		assert(controller.checkLogIn(model));
+	}
+	
+	@Test
+	public void testImportCSV() {
+		List<UserModel> users = controller.importCSV();
+		assert(users.get(0).getUsername().equals("test"));
+	}
+	
 }
