a8af291f6b9f83b417fefb9372f7de82504f35b3
Ryan Black <rblack5@ycp.edu>
2021-04-06 20:55:23 -0400

    CreateModel purge

--- CS320_Project_Database/src/project_database/servlet/CreateServlet.java
+++ CS320_Project_Database/src/project_database/servlet/CreateServlet.java
@@ -7,9 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import project_database.controller.CreateController;
 import project_database.controller.PostController;
-import project_database.model.CreateModel;
 import project_database.model.PostModel;
 
 public class CreateServlet extends HttpServlet {
