bb613cda82391cca58fb35633c269abb275558cf
Ryan Black <rblack5@ycp.edu>
2021-04-07 11:59:02 -0400

    Post Tests

--- CS320_Project_Database/junit/project_database/controller/PostControllerTest.java
+++ CS320_Project_Database/junit/project_database/controller/PostControllerTest.java
@@ -1,5 +1,43 @@
 package project_database.controller;
 
-public class PostControllerTest {
+import java.util.List;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import project_database.model.PostModel;
+
+public class PostControllerTest {
+	private PostModel model;
+	private PostController controller;
+	
+	@Before
+	public void setUp() {
+		model = new PostModel();
+		controller = new PostController();
+	}
+	
+	@Test
+	public void testSetModel() {
+		this.model.setUserID(10);
+		controller.setModel(model);
+		
+		assert(controller.getModel().getUserID() == 10);
+		
+		this.model.setUserID(12);
+		controller.setModel(model);
+		
+		assert(controller.getModel().getUserID() == 12);
+	}
+	
+	@Test
+	public void testImportCSV() {
+		List<PostModel> posts = controller.importCSV();
+		
+		assert(posts.get(0).getPostID() == 0);
+	}
+	
+	public void testFindPost() {
+		assert(controller.findPost(0).getTitle() == "ExamplePost");
+	}
 }
