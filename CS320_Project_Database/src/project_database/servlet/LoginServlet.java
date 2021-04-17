bd89caa90b3154db534f57e165d70be4895e8d0a
darkloor <rdg2250@yahoo.com>
2021-04-17 16:52:47 -0400

    weird issue fixed

--- CS320_Project_Database/src/project_database/servlet/LoginServlet.java
+++ CS320_Project_Database/src/project_database/servlet/LoginServlet.java
@@ -9,10 +9,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import project_database.model.UserModel;
 import project_database.controller.LoginController;
 import project_database.controller.PostController;
 import project_database.model.PostModel;
-import project_database.model.UserModel;
+
 
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
